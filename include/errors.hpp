4789cf31f1c6f16444341db19a3ba88868d18464
Lyubomir Gardev <rolice@Lyubo-PC.lan>
2014-09-03 20:26:29 +0300

    Modification applied for separation.

---
+++ include/errors.hpp
@@ -0,0 +1,8 @@
+#ifndef __ERRORS_HPP__
+#define __ERRORS_HPP__
+
+#define         ERR_NONE                        0
+#define         ERR_SYSTEM                      1
+#define         ERR_NETWORK                     2
+
+#endif
---
+++ include/network.hpp
@@ -0,0 +1,10 @@
+#ifndef __NETWORK_HPP__
+#define __NETWORK_HPP__
+
+#define         YES                     1
+#define         NO                      0
+
+#define         SOCK_PORT               7733
+#define         SOCK_BACKLOG            5
+
+#endif
